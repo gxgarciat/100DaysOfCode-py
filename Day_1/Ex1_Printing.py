d6d05cf1402dbbe17bc345ccdbeb100e9128b2ad
gxgarciat <info@gxgarciat.dev>
2023-01-24 13:44:05 +0000

    Day1

--- Day_1/Ex1_Printing.py
+++ Day_1/Ex1_Printing.py
@@ -1,9 +1,12 @@
 # Write your code below this line 👇
 
+# Write a program in main.py that prints the same notes from the
+# previous lesson using what you have learnt about the Python print function.
+
 print('Day 1 - Python Print Function')
 print('The function is declared like this:')
-print("print('what to print')")
+print("print('what to print')")  # Using single and double quotes
 
 
-#if __name__ == '__main__':
+# if __name__ == '__main__':
 #    main()
---
+++ Day_1/Ex3_inputFunction.py
@@ -0,0 +1,14 @@
+# Write your code below this line 👇
+
+# Write a program that prints the number of characters in a user's name.
+# You might need to Google for a function that calculates the length of a string.
+
+username = input("What is your name? ")
+size = len(username)
+print(size)
+
+# Additional
+print("Hi " + username + ". Your name has " + str(size) + " letters.")
+
+# if __name__ == '__main__':
+#    main()
