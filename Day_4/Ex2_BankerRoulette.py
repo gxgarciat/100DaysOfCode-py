567c013c7c063f3c68713a3cc9fe2181619a0512
gxgarciat <info@gxgarciat.dev>
2023-01-27 14:03:10 +0000

    Day4

--- Day_4/Ex2_BankerRoulette.py
+++ Day_4/Ex2_BankerRoulette.py
@@ -5,11 +5,13 @@ names_string = input("Give me everybody's names, separated by a comma. ")
 names = names_string.split(", ")
 # 🚨 Don't change the code above 👆
 
+# You are going to write a program that will select a random name from a list of names.
+# The person selected will have to pay for everybody's food bill.
+
 # Write your code below this line 👇
 
-
-
-
+chosenOne = random.randint(0, len(names)-1)
+print(f"{names[chosenOne]} is going to buy the meal today!")
 
 # if __name__ == '__main__':
 #     main()
