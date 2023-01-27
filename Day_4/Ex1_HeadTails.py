68b330306771b22b78b8eb61be1ccd9faf126963
gxgarciat <info@gxgarciat.dev>
2023-01-27 12:37:35 +0000

    Day4

---
+++ Day_4/Ex1_HeadTails.py
@@ -0,0 +1,15 @@
+# Remember to use the random module
+# Hint: Remember to import the random module here at the top of the file. 🎲
+
+# You are going to write a virtual coin toss program.
+# It will randomly tell the user "Heads" or "Tails".
+# Important, the first letter should be capitalised and
+# spelt exactly like in the example e.g. Heads, not heads.
+
+# Write the rest of your code below this line 👇
+
+
+
+
+# if __name__ == '__main__':
+#     main()
