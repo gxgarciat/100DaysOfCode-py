334524db5927705b8d6e0c395f0a3a8686d383bb
gxgarciat <info@gxgarciat.dev>
2023-01-30 12:33:50 +0000

    Day5

--- Day_5/Ex1_AverageHeight.py
+++ Day_5/Ex1_AverageHeight.py
@@ -4,13 +4,23 @@ for n in range(0, len(student_heights)):
   student_heights[n] = int(student_heights[n])
 # 🚨 Don't change the code above 👆
 
+# You are going to write a program that calculates the average student height from a List of heights.
+# e.g. student_heights = [180, 124, 165, 173, 189, 169, 146]
+# The average height can be calculated by adding all the heights together and dividing by the total number of heights.
+# 180 + 124 + 165 + 173 + 189 + 169 + 146 = 1146
+# There are a total of 7 heights in student_heights
 
 # Write your code below this row 👇
 
+size = 0
+sum = 0
 
+for i in student_heights:
+  sum = sum + i
+  size = size + 1
 
-
-
+average = round( sum / size)
+print(average)
 
 
 # if __name__ == '__main__':
