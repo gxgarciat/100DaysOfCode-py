247f1b2336bbcf644838d9aa7ef3d09efce3c595
gxgarciat <info@gxgarciat.dev>
2023-01-31 17:15:52 +0000

    Day7

--- Dir_7/ProjectD7_Hangman.py
+++ Dir_7/ProjectD7_Hangman.py
@@ -3,9 +3,9 @@
 
 word_list = ["aardvark", "baboon", "camel"]
 
-# TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
-# TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-# TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+# Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+# Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+# Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
 
 # if __name__ == '__main__':
 #     main()
