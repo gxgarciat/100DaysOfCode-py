e575b5ded1bbc4c63918f7d7f9a2b16877dc689a
gxgarciat <info@gxgarciat.dev>
2023-02-01 13:00:21 +0000

    Day7

--- Dir_7/ProjectD7_Hangman.py
+++ Dir_7/ProjectD7_Hangman.py
@@ -3,6 +3,57 @@ import random
 
 word_list = ["aardvark", "baboon", "camel"]
 
+hangmanpics = ['''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========''']
+
 # Randomly choose a word from the word_list and assign it to a variable called chosen_word.
 # Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
 # Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
@@ -13,8 +64,8 @@ print(chosen_word)
 userSelection = input("Guess a letter: ")
 userSelection = userSelection.lower()
 
-mistakes = chosen_word.count(userSelection)
-print(mistakes)
+userGuess = chosen_word.count(userSelection)
+print(userGuess,hangmanpics[0],hangmanpics[4])
 
 # if __name__ == '__main__':
 #     main()
